b65f24d03f4ba65be8f47fa7d585d2d5144d17af
shaik zoheb ahmed <91328904+shaikzohebahmed@users.noreply.github.com>
2023-08-21 06:32:09 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,20 +1,30 @@
-import paralleldots
+import paralleldots  # Import the paralleldots module for accessing the ParallelDots API
 
 class API:
-    
+
     def __init__(self):
-        paralleldots.set_api_key('8mxbRbNTQPRb4hRIaDwmPRDPWkogspOpXMhSv2Cz2ok')
+        """
+        Initializes the API class with the ParallelDots API key.
+        """
+        paralleldots.set_api_key('YOUR_PARALLELDOTS_API_KEY')
         
-        
-    def sentiment_analysis(self,text):
+    def sentiment_analysis(self, text):
+        """
+        Performs sentiment analysis on the provided text.
+        """
         response = paralleldots.sentiment(text)
         return response
     
-    def ner(self,text):
+    def ner(self, text):
+        """
+        Performs named entity recognition (NER) analysis on the provided text.
+        """
         response = paralleldots.ner(text)
         return response
         
-
-    def emotion_prediction(self,text):
+    def emotion_prediction(self, text):
+        """
+        Performs emotion prediction on the provided text.
+        """
         response = paralleldots.emotion(text)
         return response
