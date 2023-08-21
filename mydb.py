370b7c830d5da1c65e6a79bcdaf37b44ba785cd5
shaik zoheb ahmed <91328904+shaikzohebahmed@users.noreply.github.com>
2023-08-21 06:32:55 +0530

    Update mydb.py

--- mydb.py
+++ mydb.py
@@ -1,31 +1,34 @@
-import json
+import json  # Import the JSON module to work with JSON files
 
 class Database:
-    
-    def add_data(self,name,email,password):
-        
-        with open('db.json','r') as rf:
+
+    def add_data(self, name, email, password):
+        """
+        Adds user data to the database if the email is not already registered.
+        """
+        with open('db.json', 'r') as rf:
             database = json.load(rf)
-            
         
         if email in database:
-            return 0
+            return 0  # Email is already registered
         else:
-            database[email] = [name,password]
-            with open('db.json','w') as wf:
-                json.dump(database,wf)
-            return 1
+            # Add user data to the database
+            database[email] = [name, password]
+            with open('db.json', 'w') as wf:
+                json.dump(database, wf)
+            return 1  # Data added successfully
         
-    def search_data(self,email,password):
-        
-        with open('db.json','r') as rf:
+    def search_data(self, email, password):
+        """
+        Searches for user data in the database and checks if the provided password is correct.
+        """
+        with open('db.json', 'r') as rf:
             database = json.load(rf)
             
-        
         if email in database:
             if database[email][1] == password:
-                return 1
+                return 1  # Email and password match
             else:
-                return 0
+                return 0  # Password is incorrect
         else: 
-            return 0
+            return 0  # Email not found
